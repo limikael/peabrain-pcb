a9958dd2c08867134e5373657d564b5658258fd7
Mikael Lindqvist <li.mikael@gmail.com>
2026-03-28 09:40:15 +0800

    kisch and jlcc

---
+++ jlcmotor/jlcmotor.js
@@ -0,0 +1,61 @@
+import {declareResistor, declarePinHeader, declareEsp32,
+		declareTja1050, declareScrewTerminal, declareDrv8825,
+		declareMp1584} from "../js/component-declarations.js";
+
+/*
+todo!
+
+- capacitor
+- limit sw w/ pullup
+- status led
+- diode (power oring)
+*/
+
+export default async function(sch, {variant}) {
+	let screw1=declareScrewTerminal(sch,"J1");
+	let screw2=declareScrewTerminal(sch,"J2");
+	let screw3=declareScrewTerminal(sch,"J3");
+	let screw4=declareScrewTerminal(sch,"J4");
+	let esp32=declareEsp32(sch,"U1","U2");
+	let tja1050=declareTja1050(sch,"U3","U4");
+	let mp1584=declareMp1584(sch,"U5","U6","U7","U8");
+	let drv8825=declareDrv8825(sch,"U9","U10");
+	let r1=declareResistor(sch,"R1",4700);
+
+	screw1.connect("GND","12V","CANH","CANL");
+	screw2.connect("GND","VMOT","","");
+
+	esp32._5v.connect("5V");
+	esp32._3v3.connect("3V3");
+	esp32.gnd.connect("GND");
+
+	tja1050.vcc.connect("5V");
+	tja1050.tx.connect(esp32.gpio5);
+	r1.connect(tja1050.rx,esp32.gpio4);
+	tja1050.gnd.connect("GND");
+	tja1050.canl.connect("CANL");
+	tja1050.canh.connect("CANH");
+
+	drv8825.en.connect(esp32.gpio6);
+	drv8825.m0.connect(esp32.gpio0);
+	drv8825.m1.connect(esp32.gpio1);
+	drv8825.m2.connect(esp32.gpio3);
+	drv8825.rst.connect("3V3");
+	drv8825.slp.connect("3V3");
+	drv8825.step.connect(esp32.gpio9);
+	drv8825.dir.connect(esp32.gpio10);
+
+	drv8825.gnd1.connect("GND");
+	drv8825.fault.connect(esp32.gpio21);
+	drv8825.a2.connect(screw4.pin(1));
+	drv8825.a1.connect(screw4.pin(2));
+	drv8825.b1.connect(screw4.pin(3));
+	drv8825.b2.connect(screw4.pin(4));
+	drv8825.gnd2.connect("GND");
+	drv8825.vmot.connect("VMOT");
+
+	mp1584.vin.connect("12V");
+	mp1584.gndin.connect("GND");
+	mp1584.gndout.connect("GND");
+	mp1584.vout.connect("5V");
+}
---
+++ js/component-declarations.js
@@ -0,0 +1,124 @@
+export function declareResistor(sch, ref, ohm) {
+    let partsByOhm={
+        330: "C23138",
+        4700: "C23162"
+    };
+
+    if (!partsByOhm[ohm])
+        throw new Error("not found!");
+
+    return sch.declare(ref,{
+        symbol: "Device:R",
+        footprint: "Resistor_SMD:R_0603_1608Metric",
+        lcsc: partsByOhm[ohm]
+    });
+}
+
+export function declarePinHeader(sch, ref, pins) {
+    switch (pins) {
+        case 2:
+            return sch.declare(ref,{
+                symbol: "Connector_Generic:Conn_01x02",
+                footprint: "Connector_PinSocket_2.54mm:PinSocket_1x02_P2.54mm_Vertical",
+                lcsc: "C49661",
+                lcscRot: 90
+            });
+            break;
+
+        case 4:
+            return sch.declare(ref,{
+                symbol: "Connector_Generic:Conn_01x04",
+                footprint: "Connector_PinSocket_2.54mm:PinSocket_1x04_P2.54mm_Vertical",
+                lcsc: "C2718488",
+                lcscRot: 90
+            });
+            break;
+
+        case 8:
+            return sch.declare(ref,{
+                symbol: "Connector_Generic:Conn_01x08",
+                footprint: "Connector_PinSocket_2.54mm:PinSocket_1x08_P2.54mm_Vertical",
+                lcsc: "C27438",
+                lcscRot: 90
+            });
+            break;
+
+        default:
+            throw new Error("can't hanlde pins");
+    }
+}
+
+export function declareScrewTerminal(sch, ref, pins=4) {
+    switch (pins) {
+        case 4:
+            return sch.declare(ref,{
+                symbol: "Connector_Generic:Conn_01x04",
+                footprint: "Peabrain:ScrewTerminals_4P",
+                lcsc: "C441206",
+                lcscRot: -90,
+            });
+            break;
+
+        default:
+            throw new Error("unknown");
+    }
+}
+
+export function declareEsp32(sch, ref1, ref2) {
+    let ESP32SUPERMINI_PINNAMES=[
+        "gpio5","gpio6","gpio7","gpio8",
+        "gpio9","gpio10","gpio20","gpio21",
+        "gpio0","gpio1","gpio2","gpio3",
+        "gpio4","_3v3","gnd","_5v",
+    ];
+
+    let u1=declarePinHeader(sch,ref1,8);
+    let u2=declarePinHeader(sch,ref2,8);
+
+    return compoundSymbol(u1,u2).namePins(ESP32SUPERMINI_PINNAMES);
+}
+
+export function declareTja1050(sch, ref1, ref2) {
+    let TJA1050_PINNAMES=[
+        "vcc","tx","rx","gnd",
+        "n/a","canl","canh","n/a"
+    ];
+
+    let u1=declarePinHeader(sch,ref1,4);
+    let u2=declarePinHeader(sch,ref2,4);
+
+    return compoundSymbol(u1,u2).namePins(TJA1050_PINNAMES);
+}
+
+export function declareDrv8825(sch, ref1, ref2) {
+    let pinNames=[
+        "en","m0","m1","m2",
+        "rst","slp","step","dir",
+        "gnd1","fault","a2","a1",
+        "b1","b2","gnd2","vmot"
+    ];
+
+    let u1=declarePinHeader(sch,ref1,8);
+    let u2=declarePinHeader(sch,ref2,8);
+
+    return compoundSymbol(u1,u2).namePins(pinNames);
+}
+
+export function declareMp1584(sch, ref1, ref2, ref3, ref4) {
+    let u1=declarePinHeader(sch,ref1,2);
+    let u2=declarePinHeader(sch,ref2,2);
+    let u3=declarePinHeader(sch,ref3,2);
+    let u4=declarePinHeader(sch,ref4,2);
+
+    u1.pin(1).connect(u1.pin(2));
+    u2.pin(1).connect(u2.pin(2));
+    u3.pin(1).connect(u3.pin(2));
+    u4.pin(1).connect(u4.pin(2));
+
+    return ({
+        gndout: u1.pin(1),
+        gndin: u2.pin(1),
+        vin: u3.pin(1),
+        vout: u4.pin(1),
+    });
+}
